8b3cb6ee8b2cced60f47e93cec1e0a63d1a4c0fa
gabebuzato <gabebuzato@gmail.com>
2026-07-17 11:04:35 -0300

    feat: adiciona a rota de delete e corrige o erro para BAD REQUEST

--- ItauTesteTecnico/src/main/java/dev/gabebuzato/ItauTesteTecnico/TransacoesController.java
+++ ItauTesteTecnico/src/main/java/dev/gabebuzato/ItauTesteTecnico/TransacoesController.java
@@ -3,10 +3,7 @@ package dev.gabebuzato.ItauTesteTecnico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
@@ -15,14 +12,27 @@ public class TransacoesController {
     @Autowired
     private TransacaoService transacaoService;
 
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+
     @PostMapping
     public ResponseEntity adicionar(@RequestBody TransacaoRequest transacaoRequest) {
         try{
             transacaoService.validarTransacao(transacaoRequest);
+            transacaoRepository.salvarDados(transacaoRequest);
             return ResponseEntity.status(HttpStatus.CREATED).build();
-        } catch(IllegalArgumentException error) {
+
+        } catch(IllegalArgumentException exception) {
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        } catch(Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
 
+    }
+
+    @DeleteMapping("/deletar")
+    public ResponseEntity deletar(){
+        transacaoRepository.deletarDados();
+        return ResponseEntity.ok().build();
     }
 }
